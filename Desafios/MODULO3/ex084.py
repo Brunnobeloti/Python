ab4d5eeb55ca9f79ad75849987794ab76ab69bff
Brunno Beloti <97759915+brunnobeloti@users.noreply.github.com>
2022-07-10 20:41:43 -0300

    novos exercicios

--- Desafios/MODULO3/ex084.py
+++ Desafios/MODULO3/ex084.py
@@ -1,12 +1,39 @@
-temp = []
-dados = []
+dados = list()
+pessoas = list()
+peso_total = list()
+pessoas_maior_peso = list()
+pessoas_menor_peso  = list()
 
-while True:
-    temp.append(str(input('Nome ')))
-    temp.append(float(input('Peso: ')))
-    dados.append(temp[:])
-    temp.clear()
-    pergunta = str(input('Quer continuar? [S/N] '))
-    if pergunta in 'Nn':
+#Dados inseridos pelo usuário
+while True: 
+    dados.append(str(input('Nome: ')).strip().capitalize())
+    dados.append(float(input('Peso: ')))
+    pessoas.append(dados[:])
+    dados.clear()
+    while True:
+        proximo = str(input('Quer continuar? (S/N):')).strip().upper()
+        if proximo in 'SN':
+            break
+    if proximo == 'N':
         break
-print(dados)
+
+#Separando todos os pesos na lista > peso_total
+for peso in pessoas:
+    peso_total.append(peso[1])
+
+#Checando o peso máximo e mínimo 
+maior_peso = max(peso_total)
+menor_peso = min(peso_total)
+
+#Identificando os respectivos nomes de peso Max/Min
+for pes in pessoas:
+    if pes[1] == maior_peso: # if True: Nome, peso > Max
+        pessoas_maior_peso.append(pes[0]) 
+    if pes[1] == menor_peso: #if True: Nome, peso > Min
+       pessoas_menor_peso.append(pes[0]) 
+
+print(f'{"=-"*20}\nAo todo, você cadastrou {len(pessoas)} pessoa{"s" if len(pessoas) > 1 else ""}.')
+
+print(f'O maior peso foi de {maior_peso}Kg. Peso de {pessoas_maior_peso}')
+
+print(f'O menor peso foi de {menor_peso}Kg. Peso de {pessoas_menor_peso}')
